7a7180bf4b491427d9bc633257d29f51d3ae003a
Кузьмина Ульяна <kuzmina_ua@utisitgroup.ru>
2024-09-05 14:13:53 +0300

    Добавлена обработка alert

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,9 +5,13 @@ from .login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
+        # link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        # link.click()
         # return LoginPage(browser=self.browser, url=self.browser.current_url)
+        link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
 
     def should_be_login_link(self):
